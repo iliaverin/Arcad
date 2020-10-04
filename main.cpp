5314aca6195970ee93275ef79d4f96cce1164d70
iliaverin <58372142+iliaverin@users.noreply.github.com>
2020-10-04 22:52:40 +0300

    left-right

--- main.cpp
+++ main.cpp
@@ -1,60 +1,167 @@
 #include <iostream>
 #include <vector>
-#include <iomanip>
-#include <windows.h>
-#include <conio.h>
+
+
 using namespace std;
+class Game
+{
+private:
+    int move;
+    const int X = 30;
+    const int Y = 20;
+    vector<vector<char>> field;
+public:
+    void right();
+    void fieldcreation();
+    void printfield();
+    void start();
+    void addMet();
+    void down();
+    void left();
+};
 
-int move2;
-void fieldcreation(string m[10][10]){
 
-    cout<<"-------------------";
 
-    for (int i = 0;i < 10; i++) {
-        for (int j = 0; j < 10; ++j) {
-                m[i][j] = " ";
-                m[9][4] = "*";
-                m[9][5] = "*";
-                //cout.width(5);
-                //cout<<"|";
-                //v.push_back(10)
-                cout<<m[i][j];
-    }
+void Game::fieldcreation()
+{
 
-        cout << "\n";
 
-    }
-    cout<<"-------------------";
-}
-void moveleft(string m[10][10]){
+    for (int i = 0; i < X; i++) {
+        vector<char> a;
+        for (int j = 0; j < Y; ++j) {
 
-    for (int i = 0;i < 10; i++) {
-        for (int j = 0; j < 10; ++j) {
 
-                //cout.width(5);
-                //cout<<"|";
-                //v.push_back(10)
-                cout<<m[i][j];
-    }
-       }
-}
-void mover(){
-    int code = getch();
-        if (code == 0 || code == 224)
-            code = getch();
-        switch (code)
-        {
-        case 75://âëåâî
-            move2 = 1;
-            break;
-        case 77://âïðàâî
-            move2 = 2;
-            break;
+            a.push_back(' ');
+
+
         }
+        field.push_back(a);
+
+    }
+
+}
+
+void Game::printfield()
+{
+    for (int i = 0; i < X; i++) {
+        vector<char> a;
+        for (int j = 0; j < Y; ++j) {
+
+
+            cout << field[i][j];
+
+
+        }
+        cout<<"\n";
+
+    }
+}
+
+void Game::start()
+{
+    for (int i = 0; i < X; i++)
+    {
+        for (int j = 0; j < Y; j++)
+        {
+            field[28][10] = '+';
+            field[29][11] = '+';
+            field[28][11] = '+';
+            field[29][10] = '+';
+
+        }
+    }
+}
+
+void Game::addMet()
+{
+    for (int i = 0; i < X; i++)
+    {
+        for (int j = 0; j < Y; j++)
+        {
+            int met = (rand() % 15 + 1);
+            field[0][met] = '*';
+
+        }
+
+    }
+}
+
+void Game::down()
+{
+    for (int i = 0; i < X; i++)
+    {
+        for (int j = 0; j < Y; j++)
+        {
+            if (field[i][j] == '*')
+            {
+                int r;
+                r = field[i][j];
+                field[i + 1][j] = r;
+                field[i][j] = ' ';
+            }
+        }
+    }
+}
+
+void Game::left()
+{
+    for (int i = 0; i < X; i++)
+    {
+        for (int j = 0; j < Y; j++)
+        {
+            if (field[i][j]== '*')
+            {
+                char temp = field[i][j];
+                field[i + 1][j] = temp;
+                field[i][j] = ' ';
+                move = 1;
+            }
+
+        }
+    }
+}
+void Game::right()
+{
+    for (int i = 0; i < X; i++)
+    {
+        for (int j = 0; j < Y; j++)
+        {
+            if (field[i][j--]== '*')
+            {
+                char temp = field[i][j];
+                field[i - 1][j] = temp;
+                field[i][j] = ' ';
+                move = 1;
+            }
+
+        }
+    }
+}
+
+
+void mover() {
+    //int code = getch();
+    //if (code == 0 || code == 224)
+    //    code = getch();
+    //switch (code)
+    //{
+    //case 75://âëåâî
+    //    move2 = 1;
+    //    break;
+   // case 77://âïðàâî
+    //   move2 = 2;
+    //    break;
+    //}
 }
 int main()
 {
-    string m[10][10];
-    cout<<"1"<<endl;
-    fieldcreation(m);
+
+    Game go;
+
+    go.fieldcreation();
+    go.start();
+    go.printfield();
+    go.addMet();
+    go.left();
+
 }
